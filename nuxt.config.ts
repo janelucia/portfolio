2b85ce17d32a82250136bd9bf540088390865e0f
Jane <janeluciaschoenfeld@gmail.com>
2023-11-16 14:32:15 +0100

    add daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['daisyui'],
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content'],
   nitro: {
     prerender: {
       failOnError: false,
     },
+  },
+  daisyui: {
+    themes: ['forest', 'autumn'],
+    darkTheme: 'forest',
   },
   content: {
     highlight: {
