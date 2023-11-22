3f8a653adf9ab3123ae820dac78be91f84c05c59
Jane <janeluciaschoenfeld@gmail.com>
2023-11-22 08:21:20 +0100

    change alignment of navbar buttons

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex justify-between text-lg p-4">
+  <div
+    class="flex justify-center sm:justify-between text-lg p-4 flex-wrap gap-4 max-w-screen-lg mx-auto"
+  >
     <NuxtLink v-for="nav in navigation" :to="nav.href" :key="nav">
       <button
         class="text-lg px-4 py-2 border border-solid border-primary rounded-md hover:bg-primary hover:text-neutral"
