22536978e3217bab96dbc25b4100060f85899718
jane <janeluciaschoenfeld@gmail.com>
2023-12-24 14:04:01 +0100

    feat: add a picture to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,41 +1,44 @@
 <template>
   <section class="py-8">
-    <h2 class="text-6xl text-secondary">About</h2>
-    <div class="flex">
-      <div
-        class="bg-neutral w-full text-xl p-4 rounded-md z-10 flex flex-col gap-2"
-      >
-        <div>
-          <p class="font-bold">Name</p>
-          <p>Jane Schönfeld</p>
+    <div class="flex gap-4">
+      <div class="flex flex-col self-end w-1/2 gap-2">
+        <h2 class="text-6xl text-secondary">About</h2>
+        <div class="z-10 p-4 text-xl rounded-md bg-neutral">
+          <div>
+            <p class="font-bold">Name</p>
+            <p>Jane Schönfeld</p>
+          </div>
+          <div>
+            <p class="font-bold">Age</p>
+            <p>26</p>
+          </div>
+          <div>
+            <p class="font-bold">Location</p>
+            <p>Kiel, Germany</p>
+          </div>
+          <ul>
+            <p class="font-bold">Education</p>
+            <li>
+              B.A. Educational science and sociology,
+              Christian-Albrecht-University Kiel (1.5)
+            </li>
+            <li>
+              B.Sc. in Computer Science since 2022, University of Applied
+              Sciences Kiel (currently: 1.9)
+            </li>
+          </ul>
+          <div>
+            <p class="font-bold">Work</p>
+            <p>working student in software development since feb 2022</p>
+          </div>
+          <div>
+            <p class="font-bold">Interests</p>
+            <p>Rowing, Reading, Coding</p>
+          </div>
         </div>
-        <div>
-          <p class="font-bold">Age</p>
-          <p>26</p>
-        </div>
-        <div>
-          <p class="font-bold">Location</p>
-          <p>Kiel, Germany</p>
-        </div>
-        <ul>
-          <p class="font-bold">Education</p>
-          <li>
-            B.A. Educational science and sociology,
-            Christian-Albrecht-University Kiel (1.5)
-          </li>
-          <li>
-            B.Sc. in Computer Science since 2022, University of Applied Sciences
-            Kiel (currently: 1.9)
-          </li>
-        </ul>
-        <div>
-          <p class="font-bold">Work</p>
-          <p>working student in software development since feb 2022</p>
-        </div>
-        <div>
-          <p class="font-bold">Interests</p>
-          <p>Rowing, Reading, Coding</p>
-        </div>
+      </div>
+      <div class="w-1/2">
+        <img src="/images/jane-business.jpg" alt="" class="rounded-md" />
       </div>
     </div>
   </section>
