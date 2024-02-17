87bca0c8ca7c2e5feedca1ef3b42f8f0d342e9e7
jane <janeluciaschoenfeld@gmail.com>
2024-02-17 15:35:06 +0100

    fix: navigation buttons

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="flex justify-center sm:justify-between text-lg p-4 flex-wrap gap-4 max-w-screen-lg mx-auto"
+    class="flex flex-wrap justify-center max-w-screen-lg gap-4 p-4 mx-auto text-lg sm:justify-between"
   >
     <NuxtLink v-for="nav in navigation" :to="nav.href" :key="nav">
       <button
-        class="text-lg px-4 py-2 border border-solid border-primary rounded-md hover:bg-primary hover:text-neutral"
+        class="px-4 py-2 text-lg border border-solid rounded-md border-primary hover:bg-primary hover:text-neutral"
         :class="nav.current ? 'bg-primary text-neutral' : 'text-primary'"
       >
         {{ nav.name }}
@@ -17,12 +17,12 @@
 const route = useRoute();
 
 const navigation = computed(() => [
-  { name: 'Home', href: '/', current: route.name === '/' },
-  { name: 'About Me', href: '/about', current: route.name.includes('about') },
+  { name: "Home", href: "/", current: route.name === "index" },
+  { name: "About Me", href: "/about", current: route.name.includes("about") },
   {
-    name: 'Portfolio',
-    href: '/portfolio',
-    current: route.name.includes('portfolio'),
+    name: "Portfolio",
+    href: "/portfolio",
+    current: route.name.includes("portfolio"),
   },
 ]);
 </script>
