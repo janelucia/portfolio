fc69e37f7adcd75ec547ee72eb9e55263362f09b
jane <janeluciaschoenfeld@gmail.com>
2024-02-21 00:29:47 +0100

    feat: add additional links

--- components/Feature.vue
+++ components/Feature.vue
@@ -3,21 +3,27 @@
     class="flex flex-col items-center w-full gap-4 xl:flex-row"
     :class="{ 'xl:flex-row-reverse': reverse }"
   >
-    <NuxtPicture
-      v-if="item.name === repo.name"
-      format="avif, webp"
-      :src="item.image ? item.image : '/images/showcase-not-found.png'"
-      class="w-full xl:w-1/2"
-      :img-attrs="{ class: 'rounded-lg w-full' }"
-    />
+    <a :href="repo.html_url" class="w-full xl:w-1/2" target="_blank">
+      <NuxtPicture
+        v-if="item.name === repo.name"
+        format="avif, webp"
+        :src="item.image ? item.image : '/images/showcase-not-found.png'"
+        :img-attrs="{ class: 'rounded-lg w-full' }"
+      />
+    </a>
     <div class="xl:w-1/2">
       <div class="flex justify-between gap-4">
-        <h4 class="text-2xl font-bold text-primary">{{ repo.name }}</h4>
+        <h4 class="text-2xl font-bold text-primary">
+          <a :href="repo.html_url" class="hover:underline" target="_blank">{{
+            repo.name
+          }}</a>
+        </h4>
         <a
+          v-if="repo.homepage"
           :href="repo.homepage"
           target="_blank"
           class="text-xl hover:text-primary hover:underline"
-          >{{ repo.homepage ? "Homepage" : "Source Code" }}</a
+          >Look at me!</a
         >
       </div>
       <p v-if="item.name === repo.name" class="text-lg">
