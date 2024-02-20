1fd6839f05e5bbb95fa809dc980be4215682bfea
jane <janeluciaschoenfeld@gmail.com>
2024-02-21 00:28:54 +0100

    fix: github link

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,9 +1,9 @@
 <template>
   <footer>
     <section
-      class="mx-auto max-w-7xl px-2 sm:px-4 lg:py-6 text-gray-300 flex items-center justify-end"
+      class="flex items-center justify-end px-2 mx-auto text-gray-300 max-w-7xl sm:px-4 lg:py-6"
     >
-      <div class="flex justify-center gap-4 mb-4 mt-2">
+      <div class="flex justify-center gap-4 mt-2 mb-4">
         <a v-for="link in links" :key="link.name" :href="link.href">
           <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
         </a>
@@ -14,11 +14,11 @@
 
 <script setup>
 const links = [
-  { name: 'Github', href: 'https://github.com/', icon: 'uil:github' },
+  { name: "Github", href: "https://github.com/janelucia", icon: "uil:github" },
   {
-    name: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/jane-sch%C3%B6nfeld/',
-    icon: 'uil:linkedin',
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/jane-sch%C3%B6nfeld/",
+    icon: "uil:linkedin",
   },
 ];
 </script>
